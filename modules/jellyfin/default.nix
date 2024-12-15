d9c85f9957c2e91a7918915151cbf811c12b8d90
Matthijs Klasens <matthijs.klasens@gmail.com>
2024-12-15 22:45:11 +0100

    Jellyseerr on nixos

--- modules/jellyfin/default.nix
+++ modules/jellyfin/default.nix
@@ -9,7 +9,7 @@ with lib; let
 
     (final: prev: {
       jellyseerr = prev.jellyseerr.overrideAttrs (old: {
-        src = builtins.fetchFromGitHub {
+        src = prev.fetchFromGitHub {
           owner = "Fallenbagel";
           repo = "jellyseerr";
           rev = "v2.1.0";
