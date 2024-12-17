f3463a657c114e002dca36cfea30baccf90db510
Matthijs Klasens <matthijs.klasens@gmail.com>
2024-12-17 19:58:36 +0100

    Enable Jellperr

--- hosts/bois-server/configuration.nix
+++ hosts/bois-server/configuration.nix
@@ -32,7 +32,7 @@
     bois-nas.enable = true;
     plex.enable = true;
     plexx = {
-      enable = true;
+      enable = false;
       plexxUid = toString config.users.users.plex.uid;
       plexxGid = toString config.users.groups.plex.gid;
     };
@@ -40,7 +40,7 @@
       enable = true;
       user = "plex";
       jellperr = {
-        enable = false;
+        enable = true;
         jellperrUid = toString config.users.users.plex.uid;
         jellperrGid = toString config.users.groups.plex.gid;
       };
