a92687a6e750a80196a1c76500258893335135ed
unknown <thijs.l.blaauw@gmail.com>
2025-01-19 14:11:42 +0100

    made nextcloud module

--- modules/nextcloud/default.nix
+++ modules/nextcloud/default.nix
@@ -9,13 +9,15 @@ in {
     };
   };
 
-    services.nextcloud = {
-    enable = true;
-    package = pkgs.nextcloud28;
-    hostName = "192.168.1.100";
-    config.adminpassFile = "/etc/nextcloud-admin-pass";
-    config.dbtype = "sqlite";
-    https = true;
-    datadir = "/mnt/StoragePool/Media/NextCloud";
-  };
+config = mkIf cfg.enable {
+        services.nextcloud = {
+            enable = true;
+            package = pkgs.nextcloud28;
+            hostName = "192.168.1.100";
+            config.adminpassFile = "/etc/nextcloud-admin-pass";
+            config.dbtype = "sqlite";
+            https = true;
+            datadir = "/mnt/StoragePool/Media/NextCloud";
+        };
+    }
 }
