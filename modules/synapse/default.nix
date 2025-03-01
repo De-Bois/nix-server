014b098f232f4ca016e6b6c767a9111fee9b7c5e
Matthijs Klasens <matthijs.klasens@gmail.com>
2025-03-01 20:05:05 +0100

    Synapse

--- modules/synapse/default.nix
+++ modules/synapse/default.nix
@@ -65,7 +65,8 @@ in {
         ];
         database.name = "psycopg2";
         registration_shared_secret_path = config.sops.secrets.registration_shared_secret.path;
-        # enable_registration = true;
+        enable_registration = true;
+        registration_requires_token = true;
       };
     };
   };
