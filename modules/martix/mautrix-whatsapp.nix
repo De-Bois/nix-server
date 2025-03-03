18ab18941f2968861a03dd538401bfd6eed46f7e
Matthijs Klasens <matthijs.klasens@gmail.com>
2025-03-03 21:17:22 +0100

    remove synapse dependencie from module as this is handled by nix's services

--- modules/martix/mautrix-whatsapp.nix
+++ modules/martix/mautrix-whatsapp.nix
@@ -9,53 +9,15 @@ in {
     };
   };
 
-  config = mkIf (cfg.enable && config.modules.matrix.synapse.enable) {
+  config = mkIf cfg.enable {
 
     services.mautrix-whatsapp = {
       enable = true;
       settings = {
-        appservice = {
-          as_token = "";
-          bot = {
-            displayname = "WhatsApp Bridge Bot";
-            username = "whatsappbot";
-          };
-          database = {
-            type = "sqlite3";
-            uri = "/var/lib/mautrix-whatsapp/mautrix-whatsapp.db";
-          };
-          hostname = "[::]";
-          hs_token = "";
-          id = "whatsapp";
-          port = 29318;
-        };
-        bridge = {
-          command_prefix = "!wa";
-          displayname_template = "{{if .BusinessName}}{{.BusinessName}}{{else if .PushName}}{{.PushName}}{{else}}{{.JID}}{{end}} (WA)";
-          double_puppet_server_map = { };
-          login_shared_secret_map = { };
-          permissions = {
-            "*" = "relay";
-          };
-          relay = {
-            enabled = true;
-          };
-          username_template = "whatsapp_{{.}}";
-        };
         homeserver = {
-          address = "http://localhost:8448";
+          address = "http://localhost:8008";
           domain = config.modules.nginx.domainName;
-        };
-        logging = {
-          min_level = "info";
-          writers = [
-            {
-              format = "pretty-colored";
-              time_format = " ";
-              type = "stdout";
-            }
-          ];
-        };
+        }
       };
     };
   };
