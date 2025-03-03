810668b3bcd33ab107bfce8171bf1904911c4417
Matthijs Klasens <matthijs.klasens@gmail.com>
2025-03-03 22:00:12 +0100

    Change bridge settings

--- modules/martix/mautrix-whatsapp.nix
+++ modules/martix/mautrix-whatsapp.nix
@@ -9,15 +9,34 @@ in {
     };
   };
 
-  config = mkIf cfg.enable {
+  config = mkIf && config.modules.matrix.synapse.enablecfg.enable {
 
     services.mautrix-whatsapp = {
       enable = true;
+      # https://github.com/element-hq/mautrix-whatsapp/blob/element-main/example-config.yaml 
       settings = {
         homeserver = {
           address = "http://localhost:8008";
           domain = config.modules.nginx.domainName;
-        }
+        };
+        appservice = {
+          database = {
+            type = "postgres";
+            # Create database and choose one 
+            uri = "postgres://user:password@host/database?sslmode=disable";
+            uri = "postgres:///dbname?host=/var/run/postgresql";
+          };
+        };
+        bridge = {
+          history_sync = {
+            backfill = false;
+          };
+          permissions = {
+            #"*" = "relay";
+            "thebois.nl" = "user";
+            "@matthijs:thebois.nl" = "admin";
+          };
+        };
       };
     };
   };
