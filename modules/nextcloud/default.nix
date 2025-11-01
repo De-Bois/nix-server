f4099d7394faac1e6cf0e1b11dbb1419eff0e001
unknown <thijs.l.blaauw@gmail.com>
2025-11-02 00:38:50 +0100

    enable app store

--- modules/nextcloud/default.nix
+++ modules/nextcloud/default.nix
@@ -16,13 +16,9 @@ config = mkIf cfg.enable {
             package = pkgs.nextcloud31;
             extraApps = {
               inherit (config.services.nextcloud.package.packages.apps) contacts calendar tasks spreed;
-              memories = pkgs.fetchNextcloudApp {
-              sha512 = "ENoy9ixn+ymLhVc2il4DwrBGyC6LMxbd2krvtOEtIXJFVfEuFZdM2No1B2arSCVJOOPilezzAgc2PIjmWwtBXmRBXp1y1bpqzDHK7wUcdUpPQ6Pd1frT3/mG0OhkFY5voHFrvBXb0oGUBeYa6HSCWQ+3/dqoAdmWE/IejNjvQjNC+7MZwna7KhE5/oDef/jUFpVucUrcgpUzv1sBX1T3nfelYe5J6nLfLSVV3hZxKFxGakc6VtSKGYh5Q1mw2EC27qW7+QsyyDy/tPqqBdb+63rU8sJ37qDzuMaWNl/IY1pH1EApo+GVFOn2Kymn0YL6jVUa31iMcO+Mf/7jrethBAvzdqixkqDzDPm54RFJ7pqQ/lslleF1n8q4W9yVCwILvj2ObOtPNvNMPN59LDSm9gXkGmluVY396owQBz30uarwDeF8MGPgb0iEmF5g/ZLSfFaZISbUvvo+jSQ8tiZEkEQP1cymxe7QTn7mO8QTB9hGKqdI07EotdjySJfyS++9GeaBjvefcZZ/91Yh8Ec8M3xo7+aqscb8EH4iWULJywNqfoPEvUwwpDJQ2dFXuoMPhzfBbskcnXU/Ov9O4kwjD+W5upsFzQcfHV5TuXtuG1D3KHlPO6a+CBLJ92kzO5dWehAengIVluYW5TVvMU3gaj2Ym8ogv3cBwK8xGvK2Xm4=";
-              url = "https://github.com/CollaboraOnline/richdocumentscode/releases/download/25.4.504/richdocumentscode.tar.gz";
-              license = "gpl3";
-              };
             };
             extraAppsEnable = true;
+            appstoreEnable = true;
             hostName = "cloud.hubclup.nl";
             https = true;
             database.createLocally = true;
