d1ea5e1a4eaf59ae89342a3530299f8b0464b108
unknown <thijs.l.blaauw@gmail.com>
2025-11-02 00:15:25 +0100

    enable NextcloudOffice

--- modules/nextcloud/default.nix
+++ modules/nextcloud/default.nix
@@ -15,7 +15,7 @@ config = mkIf cfg.enable {
             enable = true;
             package = pkgs.nextcloud31;
             extraApps = {
-              inherit (config.services.nextcloud.package.packages.apps) contacts calendar tasks onlyoffice spreed richdocumentscode;
+              inherit (config.services.nextcloud.package.packages.apps) contacts calendar tasks spreed richdocumentscode;
             };
             hostName = "cloud.hubclup.nl";
             https = true;
